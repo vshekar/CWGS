906930b0ac20bf09d464111eb7797c4f5c90b72a
jayeshkv <jvijayaraghavalu@umassd.edu>
2016-10-01 18:30:56 -0400

    Contact picker ->  toast name and number | Also your emulator needs to have contacts stored to be displayed.

--- app/src/main/java/edu/umassd/emergencycontact/MainActivity.java
+++ app/src/main/java/edu/umassd/emergencycontact/MainActivity.java
@@ -1,13 +1,18 @@
 package edu.umassd.emergencycontact;
 
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.graphics.BitmapCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,98 +20,59 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    private LinearLayout mDrawerList;
-    private Intent emergencyCallIntent;
-    private NotificationCompat.Builder mNotifyBuilder;
-    private int mnotID;
-    Context ctx;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button button = (Button) findViewById(R.id.emergencyCall);
-        mDrawerList = (LinearLayout)findViewById(R.id.navList);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                callEmergency();
+    }
+
+    public void callContacts(View v) {
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1) {
+            if(resultCode == ActionBarActivity.RESULT_OK) {
+                Uri contactData = data.getData();
+                Cursor c = getContentResolver().query(contactData,null,null,null,null);
+
+                if(c.moveToFirst()) {
+                    String id = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+                    String number = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                    if(number.equalsIgnoreCase("1")) {
+                        //using pho to query inbuilt database | ContactsContract.CommonDataKinds
+                        Cursor pho = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ id,null, null);
+
+                        pho.moveToFirst();
+                        String cNumber = pho.getString(pho.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        String nameContact = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+                        Toast.makeText(getApplicationContext(), "number = "+cNumber+" - name - "+nameContact, Toast.LENGTH_SHORT).show();
+
+
+
+                    }  else {
+
+                    }
+
+                }
             }
-        });
-
-        ctx = getApplicationContext();
-
-        NotificationManager nm = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
-
-
-        emergencyCallIntent = new Intent(this, MainActivity.class);
-        emergencyCallIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent emergencyCallPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, emergencyCallIntent, Intent.FILL_IN_ACTION);
-
-        mNotifyBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Emergency Caller")
-                .setContentText("Click to make and emergency call")
-                .setContentIntent(emergencyCallPendingIntent)
-                .setAutoCancel(true);
-        Notification not = mNotifyBuilder.build();
-        not.flags |= Notification.FLAG_AUTO_CANCEL;
-        nm.notify(mnotID,not);
-    }
-
-    protected void onNewIntent(Intent intent) {
-        callEmergency();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
         }
-        else if (id == R.id.drawer){
-            DrawerLayout drawer = (DrawerLayout) this.findViewById(R.id.drawer_layout);
-            drawer.openDrawer(mDrawerList);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void callEmergency() {
-        Calendar c = Calendar.getInstance();
-        Log.e("Hour", "Hour of day : " + c.get(Calendar.HOUR_OF_DAY) );
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-
-        if (9 < hour && hour < 17){
-            Log.e("Hour", "Work time!");
-            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "8452331851"));
-            startActivity(intent);
-        }
-        else{
-
-            Log.e("Hour", "Play Time!");
-            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "5089998596"));
-            startActivity(intent);
-
-        }
-
     }
 }
